50d90f206a90c583f48cfdf2fe173ea13a4325e0
Shilcof <robert.shilcof@gmail.com>
2021-01-26 11:23:15 +0000

    commit tree 0b91e52410969d25b5d6333e05907ecd4e2ef157

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const output = [];
+    for (let i = 0; i < names.length; i++) {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return output
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number --        
+    }
+}
